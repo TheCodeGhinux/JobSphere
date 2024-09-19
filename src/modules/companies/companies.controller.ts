f08e7a25e854e57c8c1f8e4639bebc7110af7dc9
CodeGhinux <ilesanmibry@gmail.com>
2024-09-19 06:48:24 +0100

    feat: added feature to find all companies and by ID

--- src/modules/companies/companies.controller.ts
+++ src/modules/companies/companies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
 import { CompaniesService } from './companies.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
@@ -8,13 +8,13 @@ export class CompaniesController {
   constructor(private readonly companiesService: CompaniesService) {}
 
   @Get()
-  findAll() {
-    return this.companiesService.findAll();
+  async findAll() {
+    return await this.companiesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.companiesService.findOne(id);
+  async findOne(@Param('id', new ParseUUIDPipe()) id: string) {
+    return await this.companiesService.findOne(id);
   }
 
   @Patch(':id')
